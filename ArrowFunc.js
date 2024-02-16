356297568858ca661ed16bc544f740bab0c998b1
CaitlinC11 <chr1111@live.com>
2024-02-16 11:04:41 -0500

    answers to exercise

--- ArrowFunc.js
+++ ArrowFunc.js
@@ -1,8 +1,17 @@
+const double = arr => arr.map(val => val * 2); //ES2015 one-liner version of: 
+// function double(arr) {
+    // return arr.map(function(val) {
+    //   return val * 2;
+    // });
+//   }
 
-const PI = 3.14; // ES5 to ES2015. Remove the var and replaced with const
-
-// Quiz:
-// - What is the difference between ***var*** and ***let***? You cannot redeclare let but you can reassign and declare var
-// - What is the difference between **var** and ***const***? You cannot redeclare or reassign const but you can do both with var.
-// - What is the difference between **let** and **const**?  You can not redeclare or reassign using const but you can reassign with let.
-// - What is hoisting? variables are 'hoisted' to the top of the scope they are being delcared in. Do this before they are defined.
+ const squareAndFindEvens = numbers => numbers.map(num => num ** 2).filter(square => square % 2 === 0); // ES2015 with arrow functions from the following code:
+// function squareAndFindEvens(numbers){
+//     var squares = numbers.map(function(num){
+//       return num ** 2;
+//     });
+//     var evens = squares.filter(function(square){
+//       return square % 2 === 0;
+//     });
+//     return evens;
+//   }
